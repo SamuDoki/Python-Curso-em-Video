251c17f2ec651f14927bbf98c1f4d7ba07b5bd25
Samuel <79607822+SamuDoki@users.noreply.github.com>
2021-08-13 22:28:42 -0300

    Create exercicio074.py

---
+++ Mundo 3/exercicio074.py
@@ -0,0 +1,7 @@
+from random import randint
+
+tupla = (randint(1,100), randint(1,100), randint(1,100), randint(1,100), randint(1,100),)
+
+print(f'o maior número foi {max(tupla)}\no menor número foi {min(tupla)}')
+print(f'tupla completa -> {tupla}')
+    

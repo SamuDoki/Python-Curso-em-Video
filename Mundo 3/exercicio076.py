decb9bacca03e0141d03bad6b64bc68990a5b512
Samuel <79607822+SamuDoki@users.noreply.github.com>
2021-08-14 09:39:56 -0300

    Create exercicio076.py

---
+++ Mundo 3/exercicio076.py
@@ -0,0 +1,19 @@
+lista = ('Lápis', 1.75,
+        'Borracha', 2,
+        'Caderno', 15.90,
+        'Estojo', 25,
+        'Transferidor', 4.20,
+        'Compasso', 9.99,
+        'Mochila', 120.32,
+        'Canetas', 22.30,
+        'Livro', 34.90
+        )
+
+print('--- LISTA DE PREÇOS ---')
+    
+for check in range(0, len(lista)):
+    if check % 2 == 0:
+        print(f'{lista[check]}{"." * (35 - len(lista[check]))}', end='')  #primeiro termo é as palavras // Segundo termo é multiplicar os pontos por 35 - quantas letras tem a palavra, para balancear
+    
+    else:
+        print(float(lista[check]))
